d6409c48baab7ed8d85b87850eb2eee3d76bbf6b
Abdulrahmannabil12 <Abdulrahmannabil12@gmail.com>
2021-09-17 01:29:40 +0200

    create project with mat-ui

---
+++ src/components/ui/header.js
@@ -0,0 +1,13 @@
+ import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+ 
+export default function Header(props) {
+    return(
+
+        <AppBar>
+        <Toolbar>
+        <a href="WWW.Google.COM">Olla</a>
+        </Toolbar>
+        </AppBar>
+    )
+}
